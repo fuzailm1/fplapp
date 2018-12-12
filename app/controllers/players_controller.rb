dfe0eccd0f43573857616dee814abe2beba59b53
Fuzail Misarwala <fuzail.misarwala@cerner.com>
2018-12-12 11:29:59 -0600

    Update player controller fixtures

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -4,7 +4,7 @@ class PlayersController < ApplicationController
     all_players = Player.all
     all_players.each do |player|
       player.fixture_list = ""
-        Opposition.where(player_id: player.id)[0..4].each do |opposition|
+        Opposition.where(player_id: player.id, gameweek: [17,18,19,20,21]).each do |opposition|
         player.fixture_list << (opposition.opp_short_name_string) << " "
       end
     end
