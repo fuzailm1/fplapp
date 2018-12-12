f28ab9fa23ffaeab2a445fb2639b4dad8f656f6a
Fuzail Misarwala <fuzail.misarwala@cerner.com>
2018-12-12 11:39:49 -0600

    Change player controller

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -4,7 +4,7 @@ class PlayersController < ApplicationController
     all_players = Player.all
     all_players.each do |player|
       player.fixture_list = ""
-        Opposition.where(player_id: player.id, gameweek: [17,18,19,20,21]).order('gameweek ASC')
+        Opposition.where(player_id: player.id)[0..4].order('gameweek ASC')
 .each do |opposition|
         player.fixture_list << (opposition.opp_short_name_string) << " "
       end
